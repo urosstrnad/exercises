3f07e504183526fad768ed528732d2bfec337400
urosstrnad <112725893+urosstrnad@users.noreply.github.com>
2022-09-06 11:07:43 +0200

    Whale Talk - spremeni vnos v samoglasnike
    Whale Talk
    Takes a phrase like "turpentine and turtles" and translate it into its “whale talk” equivalent, "uueeieeauuee".
    
    There are a few simple rules for translating text to whale language:
    
    There are no consonants. Only vowels excluding the letter y.
    The u‘s and e‘s are extra long, so we must double them.
    Once we have converted text to the whale language, the result is sung slowly, as is custom in the ocean.
    
    Write a whale.cpp program that accomplishes this translation using loops and vectors.

---
+++ whaleTalk.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main () {
+
+  char retry;
+  std::string sentence;
+  std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+  std::vector<char> result;
+
+  do {
+    
+    sentence = "";
+    result.clear();
+    std::cout << "Type in the sentence to translate.\nSentence: ";
+    std::cin.ignore();
+    getline(std::cin, sentence);
+ 
+    for (int i = 0; i < sentence.size(); i++) {
+
+      for (int j = 0; j < vowels.size(); j++) {
+        
+        if (sentence[i] == vowels[j] ) {
+          
+          if (vowels[j] == 'u' || vowels[j] == 'U' ||vowels[j] == 'e' ||vowels[j] == 'E') {
+
+            result.push_back(vowels[j]);
+            result.push_back(vowels[j]);
+
+          } else {
+
+            result.push_back(vowels[j]);
+
+          }        
+        }   
+      }
+    }
+
+    for (int i = 0; i < result.size(); i++) std::cout << result[i];
+
+    std::cout << "\n\nDo you wish to translate another sentence? y/n ";
+    std::cin >> retry;
+    std::cout << std::endl;
+
+
+  } while (retry == 'y' || retry == 'Y');
+
+}
