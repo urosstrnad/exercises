ea68ea9db73684b41960dc0f84957848faa371be
urosstrnad <112725893+urosstrnad@users.noreply.github.com>
2022-09-07 21:47:40 +0200

    Update fizzBuzz.cpp

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -4,7 +4,8 @@ int main() {
 
  
   for (int i = 1; i <= 100; i++){
-
+   
+    //you could also use if (i % 15 == 0)... if a number is divisible by 5 and 3 at the same time, it is divisible by 15;
     if (i % 5 == 0 && i % 3 == 0) {
       
       std::cout << "FizzBuzz\n";
